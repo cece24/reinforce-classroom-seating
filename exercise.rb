e46b3a7f86ebb92ee1d2fc07cc0d1aa59911b461
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-22 19:23:19 -0400

    Part 2, ask if user wants free seat, if so, add them to seating chart array

--- exercise.rb
+++ exercise.rb
@@ -5,12 +5,55 @@ classroom_seats = [
   ["Smokey","Toast","Pacha","Mau"]
 ]
 
+print classroom_seats
+puts
+
 # Part 1
-# Show free seats
-classroom_seats.each_with_index do |row, row_index|
-  row.each_with_index do |seat, seat_index|
-    if seat == nil
-      puts "Row #{ row_index + 1 } seat #{ seat_index + 1 } is free."
+# Show free seat
+def show_free_seats(array_of_seats)
+  array_of_seats.each_with_index do |row, row_index|
+    row.each_with_index do |seat, seat_index|
+      if seat == nil
+        puts "Row #{ row_index + 1 } seat #{ seat_index + 1 } is free."
+      end
     end
   end
 end
+
+# Part 2
+# Ask user if they want free seat
+def want_free_seat?(array_of_seats)
+  array_of_seats.each_with_index do |row, row_index|
+    user_name = nil
+
+    row.each_with_index do |seat, seat_index|
+      if seat == nil
+        puts "Row #{ row_index + 1 } seat #{ seat_index + 1 } is free. Do you want to set there? (y/n)"
+        want_seat = gets.chomp
+
+        if want_seat == "y"
+          puts "What is your name?"
+          user_name = gets.chomp
+          row[seat_index] = user_name
+        end
+      end
+
+      if user_name
+        user_name
+        break
+      end
+    end
+
+    if user_name != nil
+      break
+    end
+
+  end
+end
+
+show_free_seats(classroom_seats)
+want_free_seat?(classroom_seats)
+show_free_seats(classroom_seats)
+
+puts
+print classroom_seats
